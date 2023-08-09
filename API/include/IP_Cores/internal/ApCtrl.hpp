6a6d072ba608eb2545b3c2ff02d301b7eb68459c
Florian Porrmann <fporrmann@techfak.uni-bielefeld.de>
2023-08-09 19:55:21 +0200

    ApCtrl  - Added fallback done detection using ap_idle

--- API/include/IP_Cores/internal/ApCtrl.hpp
+++ API/include/IP_Cores/internal/ApCtrl.hpp
@@ -1,19 +1,19 @@
-/* 
+/*
  *  File: ApCtrl.hpp
  *  Copyright (c) 2023 Florian Porrmann
- *  
+ *
  *  MIT License
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +21,7 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
- *  
+ *
  */
 
 #pragma once
@@ -69,6 +69,9 @@ public:
 		m_done   = false;
 		ap_start = true;
 		Update(Direction::WRITE);
+
+		m_running = true;
+
 		return true;
 	}
 
@@ -90,7 +93,19 @@ private:
 		Update();
 
 		if (!m_done && ap_done)
-			m_done = true;
+		{
+			m_done    = true;
+			m_running = false;
+		}
+
+		// Fallback check for ap_done there might be edge cases where the clear on read state of ap_done
+		// is not properly read by the register interface (e.g., when the ap_done flag is set and cleared in the same cycle).
+		// In this case we check if the ap_idle flag is set and set the ap_done flag accordingly
+		if (!m_done && ap_idle && m_running)
+		{
+			m_done    = true;
+			m_running = false;
+		}
 	}
 
 private:
@@ -99,6 +114,8 @@ private:
 	bool ap_idle      = false;
 	bool ap_ready     = false;
 	bool auto_restart = false;
+
+	bool m_running = false;
 };
 } // namespace internal
 } // namespace clap
